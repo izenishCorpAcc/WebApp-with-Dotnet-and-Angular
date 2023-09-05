7c7dd1eb15da193e67d4e9009b17ea562b9931bc
Jenish Prajapati <jenish.prajapati@cotiviti.com>
2023-09-05 14:56:12 +0545

    Conguration and ef packages + new controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 
 
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -15,5 +16,18 @@ namespace API.Controllers
         {
             _context = context;
         }
+    
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users=_context.Users.ToList();
+            return users;
+        }
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
     }
+
 }
