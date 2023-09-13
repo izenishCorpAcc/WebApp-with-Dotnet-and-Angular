25af54ba634c46b39836eeb699bb72da17618b2c
Jenish Prajapati <jenish.prajapati@cotiviti.com>
2023-09-13 18:38:21 +0545

    Left at 57.Persisting the login

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  baseUrl='https://localhost:5001/api/v1/'
+
+  constructor(private http:HttpClient) {
+  
+   }
+
+   login(model:any){
+    return this.http.post(this.baseUrl+'account/login',model).pipe(
+      map((response:any)=>{
+        const user=response;
+        if (user){
+          localStorage.setItem('user',JSON.stringify(user))
+        }
+      })
+    )
+   }
+
+   logout(){
+    localStorage.removeItem('user');
+   }
+}
+ 
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-nav',
@@ -6,16 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
-  model:any={}
+  model: any = {}; // Initialize an empty object to store user input
+  loggedIn = false;
 
-  constructor(){}
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
-  login(){
-    console.log(this.model);
-    
+  login() {
+    // console.log(this.model);
+    // Call the login() method from the AccountService
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true; // Set loggedIn to true upon successful login
+      },
+      error: error => console.log(error)
+
+    })
+
+  }
+  logout(){
+    this.loggedIn=false;
   }
 
+
 }
+// NavComponent is an Angular component responsible for displaying a navigation bar and handling user login.
+// It imports the AccountService to use its login() method for sending login requests to the server.
+// The model object is used to store user input, likely including a username and password entered in a form.
+// The login() method is called when the user initiates a login action, such as clicking a "Login" button in the user interface.
+// Inside the login() method, the accountService.login(this.model) function is called to send a POST request to the server's login endpoint with the user's input.
+// The .subscribe() method is used to subscribe to the HTTP request's response. When a successful response is received (next), the loggedIn variable is set to true, indicating that the user is now logged in. If an error occurs during the request (error), it is logged to the console.
+// In summary, the AccountService handles HTTP requests related to account functionality, while the NavComponent is responsible for user interactions, like filling out a login form and processing the login request. Upon a successful login, the loggedIn variable is set to true.
